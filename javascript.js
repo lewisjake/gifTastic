f2ad1525882ac6937165e1900d0b6d51cf17182e
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-09-04 13:11:14 -0700

    Using ajax, I was able to go out and get the gifs with the API Key and bring them to my html page.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var videogames = ["Halo: Combat Evolved", "Halo 2", "Halo 3", "Fuzion Frenzy", "Grand Theft Auto: San Andreas", "Fable", "Amped", "Amped 2", "Fight Night Champion"];
+    var videogames = ["Halo: Combat Evolved", "Crash Bandicoot", "Mario Kart", "Call of Duty: Modern Warfare 2", "Grand Theft Auto: San Andreas", "Street Fighter", "Tekken", "Metal Slug", "Borderlands"];
     // add buttons for the videogames array
     function renderButtons() {
         $("#videogame-buttons").empty();
@@ -17,4 +17,34 @@ $(document).ready(function () {
         renderButtons();
         return;
     });
+    // using API to get gifs and putting them onto the html
+    $("button").on("click", function() {
+        var videogame = $(this).attr("data-videogame")
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + videogame + "&api_key=qf9Z3gs9Yc97VPg2MSvT46MOnLPL0FJo&limit=10"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (snapshot) {
+            var results = snapshot.data;
+            $("#videogames").empty();
+            for (var i=0; i <results.length; i++) {
+                var videogameDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var videogameImg = $("<img>");
+
+                videogameImg.attr("src", results[i].images.original_still.url);
+				videogameImg.attr("data-still", results[i].images.original_still.url);
+				videogameImg.attr("data-animate", results[i].images.original.url);
+				videogameImg.attr("data-state", "still");
+				videogameImg.attr("class", "gif");
+				videogameDiv.append(p);
+				videogameDiv.append(videogameImg);
+				$("#videogames").append(videogameDiv); 
+            }
+        });
+    });
+
+
+
+
 });
